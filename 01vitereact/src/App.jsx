1b5cdf037e6c205372b7012d492d8927cdc56928
karmveer7 <mybestdhoni@gmail.com>
2024-03-20 10:25:57 +0530

    my counter project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from './Chai'
+
+function App() {
+  
+
+  return (
+    <>
+     <Chai/>
+    <h3> This is my first chai which is made by me</h3>
+   
+    </>
+   
+  )
+}
+
+export default App
